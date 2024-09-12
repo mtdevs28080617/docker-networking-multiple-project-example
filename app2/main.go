c6aa18ab48ab33cec400e32bbf4330c990f1e31d
Matthew Thomas <matthew.thomas@mtdevs.co.uk>
2024-09-12 20:50:02 +0100

    example

---
+++ app2/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// Response structure for JSON output
+type Response struct {
+	Message string `json:"message"`
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		response := Response{Message: "pong"}
+
+		if err := json.NewEncoder(w).Encode(response); err != nil {
+			http.Error(w, "Failed to encode JSON response", http.StatusInternalServerError)
+			return
+		}
+	})
+
+	log.Fatalln(http.ListenAndServe(":80", nil))
+}
